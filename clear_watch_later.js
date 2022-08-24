374d47cba39da12c9eff22d67e4aa06120dedd08
Mauricio Mossi <mauricio.b.mossi@gmail.com>
2022-08-23 23:20:44 -0400

    first-commit

---
+++ clear_watch_later.js
@@ -0,0 +1,6 @@
+setInterval(()=>{
+  let select = document.querySelectorAll('button.style-scope.yt-icon-button[aria-label="Action menu"]')
+  select[25].click()
+  let its = document.querySelectorAll(".style-scope .ytd-menu-service-item-renderer")
+  its[8].click()
+}, 1000)
