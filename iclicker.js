08750277c2e2c3b29318d474c5597199a3a1c757
Mauricio B. Mossi <mauricio.b.mossi@gmail.com>
2024-02-13 02:38:25 -0500

    clicker added

---
+++ iclicker.js
@@ -0,0 +1,17 @@
+/**
+    * Enters clicker answers automatically.
+    * @param {string} lowerCaseOption - Enter answer letter in lowercase.
+    */
+function selectOption(lowerCaseOption, deltaTimeMilli) {
+    console.log("To option selected page.")
+
+    const interval = setInterval(function() {
+        console.log("still going...")
+        const btn = document.querySelector(`button#multiple-choice-${lowerCaseOption}`)
+        if (btn) {
+            btn.click()
+            clearInterval(interval)
+            console.log("Ended.")
+        }
+    }, deltaTimeMilli)
+}
